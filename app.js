4422fcfb634cd563d0b4bc38d8fd2b5eb3cd83ec
DevJorgito <jorgito.soh@gmail.com>
2024-09-16 21:39:03 -0500

    test bot

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const { createBot, createProvider, createFlow, addKeyword, EVENTS } = require('@bot-whatsapp/bot');
-require("dotenv").config
+require("dotenv").config();
 
 const QRPortalWeb = require('@bot-whatsapp/portal');
 const BaileysProvider = require('@bot-whatsapp/provider/baileys');
@@ -8,12 +8,11 @@ const chat = require("./chatGPT");
 const path = require("path");
 const fs = require("fs");
 
-// read queries from file
 const pathQueries = path.join(__dirname, "messages", "queries.txt");
 const promptQueries = fs.readFileSync(pathQueries, "utf-8");
 
+const state = new Map();
 
-// main flow
 const flowMain = addKeyword(['Hola', 'hola', 'hi', 'Hi', 'hello', 'Hello'])
     .addAnswer(
         'Bienvenido al soporte de *ManyToOne*\n\n' +
@@ -22,53 +21,62 @@ const flowMain = addKeyword(['Hola', 'hola', 'hi', 'Hi', 'hello', 'Hello'])
         '👉 ¿En qué puedo ayudarte hoy? Escribe *MENU* para ver las opciones disponibles.'
     );
 
-// menu options
 const menu = '👉 Selecciona una opción escribiendo el número:\n\n' +
-        '1. 🌐 Quiero saber más sobre la comunidad\n' +
-        '2. 🛠️ Tengo problemas para verificarme\n' +
-        '3. 🤖 Consultas a la IA\n' +
-        '4. 🙋‍♂️ Quiero hablar con un representante.\n'+
-        '0. 🚪 Salir';
+    '1. 🌐 Quiero saber más sobre la comunidad\n' +
+    '2. 🛠️ Tengo problemas para verificarme\n' +
+    '3. 🤖 Consultas a la IA\n' +
+    '4. 🙋‍♂️ Quiero hablar con un representante.\n' +
+    '0. 🚪 Salir';
 
-// queries flow
 const flowQueries = addKeyword(EVENTS.ACTION)
-    .addAnswer("Hola, ¿qué consulta tienes?",
-        {capture:true}, async (ctx, ctxFn ) => {
-            const prompt = promptQueries
-            const query = ctx.body
-            const answer = await chat(prompt, query)
-            await ctxFn.flowDynamic(answer.content)
-    })
+    .addAnswer("Hola, ¿qué consulta tienes?", { capture: true }, async (ctx, ctxFn) => {
+        const userState = state.get(ctx.from);
+        if (userState && userState.inConversationWithRep) {
+            await ctxFn.flowDynamic("Ya estás en conversación con un representante.");
+            return ctxFn.endFlow();
+        }
 
-// menu flow
-const flowMenu = addKeyword("MENU","menu").addAnswer(
+        const prompt = promptQueries;
+        const query = ctx.body;
+        const answer = await chat(prompt, query);
+        await ctxFn.flowDynamic(answer.content);
+    });
+
+const flowMenu = addKeyword("MENU", "menu").addAnswer(
     menu,
     { capture: true },
-    async (ctx, { gotoFlow, fallBack, flowDynamic }) => {
-        if (!["1", "2", "3", "4", "0"].includes(ctx.body)) {
-            return fallBack(
-                "Respuesta no válida, por favor selecciona una de las opciones."
-            );
+    async (ctx, { gotoFlow, fallBack, flowDynamic, endFlow }) => {
+        const userState = state.get(ctx.from);
+        
+        if (userState && userState.inConversationWithRep) {
+            await flowDynamic("Ya estás en conversación con un representante.");
+            return endFlow();
         }
+        
+        if (!["1", "2", "3", "4", "0"].includes(ctx.body)) {
+            return fallBack("Respuesta no válida, por favor selecciona una de las opciones.");
+        }
+
         switch (ctx.body) {
             case "1":
-                return await flowDynamic('MTO o ManyToOne es una comunidad creada por estudiantes, enfocada en el aprendizaje colaborativo ' +
-                                      'y el crecimiento en áreas como desarrollo web, redes, robótica y ciberseguridad. Ofrecemos un entorno de apoyo mutuo ' +
-                                      'donde los miembros podemos aprender juntos, compartir conocimientos y enfrentar desafíos en estos campos, la idea es ' +
-                                      'facilitar el aprendizaje práctico y obtener experiencia.\n\n' +
-                                      '👉 Escribe *MENU* para ver las opciones disponibles.');
+                return await flowDynamic(
+                    'MTO o ManyToOne es una comunidad creada por estudiantes, ' +
+                    '👉 Escribe *MENU* para ver las opciones disponibles.'
+                );
             case "2":
-                return await flowDynamic('Si estás teniendo problemas para verificarte en Discord, asegúrate de que estás siguiendo correctamente el enlace ' +
-                                         'de verificación y de que no hay errores tipográficos en la información que ingresaste. Si el problema persiste, ' +
-                                         'intenta reiniciar el proceso. Además, revisa tu carpeta de spam o correo no deseado, ya que a veces los correos ' +
-                                         'electrónicos pueden llegar allí. Si tienes otro problema solicita hablar con un representante.\n\n' +
-                                         '👉 Escribe *MENU* para ver las opciones disponibles.');
+                return await flowDynamic(
+                    'Si estás teniendo problemas para verificarte en Discord, ... ' +
+                    '👉 Escribe *MENU* para ver las opciones disponibles.'
+                );
             case "3":
-                return gotoFlow(flowQueries); 
+                return gotoFlow(flowQueries);
             case "4":
-                return await flowDynamic('En breve, te conectaremos con un representante que podrá asistirte. ' +
-                    'Por favor, mantente atento, ya que te responderán a la mayor brevedad posible. ¡Agradecemos tu paciencia!');             
-                    
+                state.set(ctx.from, { inConversationWithRep: true });
+                await flowDynamic(
+                    'En breve, te conectaremos con un representante. ' +
+                    'Por favor, mantente atento, ya que te responderán a la mayor brevedad posible.'
+                );
+                return endFlow();
             case "0":
                 return await flowDynamic(
                     "Saliendo... Puedes volver a acceder a este menú escribiendo '*Menu'"
@@ -77,7 +85,18 @@ const flowMenu = addKeyword("MENU","menu").addAnswer(
     }
 );
 
-// main function to start
+
+async function handleIncomingMessage(ctx) {
+    const userState = state.get(ctx.from);
+
+    if (userState && userState.inConversationWithRep) {
+        console.log(`Usuario ${ctx.from} está en conversación con un representante. No se procesará el mensaje.`);
+        return;
+    }
+
+    console.log(`Mensaje recibido del usuario ${ctx.from}: ${ctx.body}`);
+}
+
 const main = async () => {
     const adapterDB = new JsonFileAdapter();
     const adapterFlow = createFlow([flowMain, flowMenu, flowQueries]);
@@ -87,10 +106,10 @@ const main = async () => {
         flow: adapterFlow,
         provider: adapterProvider,
         database: adapterDB,
+        onMessage: handleIncomingMessage
     });
 
     QRPortalWeb();
 };
-
 
 main();
